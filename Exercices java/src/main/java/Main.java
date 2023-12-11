83990bd2748be2bdbfc918f5e44b2e7dd2f67e70
Arheee <125261469+Arheee@users.noreply.github.com>
2023-12-11 16:25:30 +0100

    Exercice 3 avant correction

--- Exercices java/src/main/java/Main.java
+++ Exercices java/src/main/java/Main.java
@@ -1,13 +1,16 @@
 import exercices.tp1.exercice1.CalculerMoyenne;
 import exercices.tp1.exercice2.PrixTtc;
+import exercices.tp1.exercices3.Palindrome;
 
 public class Main {
 
     public static void main(String[] args) throws IllegalAccessException{
-        float prixHorsTaxes = 100;
-        double tauxTva = 0.20;
-
-        double prixToutesTaxesComprises = prixHorsTaxes + (prixHorsTaxes*tauxTva);
-        System.out.println("le prix TTC est " + prixToutesTaxesComprises);
+        int nombre = 121;
+        Palindrome palindromeChecker = new Palindrome();
+        if(palindromeChecker.estPalindrome(nombre)){
+            System.out.println(nombre + "est un palindrome");
+        }else {
+            System.out.println(nombre + "n'est pas un plaindrome");
+        }
     }
 }
--- Exercices java/src/main/java/exercices/tp1/exercices3/Palindrome.java
+++ Exercices java/src/main/java/exercices/tp1/exercices3/Palindrome.java
@@ -1,4 +1,26 @@
 package exercices.tp1.exercices3;
 
 public class Palindrome {
+
+    public boolean estPalindrome(int nombre) throws  IllegalAccessException{
+    int nombreDeBase = nombre;
+    int nombreInverse = 0;
+
+    while (nombre > 0){
+        int reste = nombre % 10;
+        nombreInverse = nombreInverse * 10 + reste;
+        nombre = nombre / 10;
+    }
+    return nombreDeBase == nombreInverse;
+
+    }
+
+    private static void verifierSiLeNombreEstPositif(int nombre) throws IllegalAccessException{
+        if(nombre < 0){
+        System.out.println("Le nombre doit être supérieur à 0 stp");
+        }
+    }
+
+
 }
+
